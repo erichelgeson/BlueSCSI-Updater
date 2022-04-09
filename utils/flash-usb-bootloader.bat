9e03ed67e9b51085e489db47af5e743254de298e
Eric Helgeson <erichelgeson@gmail.com>
2022-04-09 13:26:12 -0500

    Fix path for windows

--- utils/flash-usb-bootloader.bat
+++ utils/flash-usb-bootloader.bat
@@ -6,7 +6,7 @@ pause
 echo Starting flash...
 echo NOTE: Some BulePills can take up to 2 minutes to flash.
 
-utils\openocd\xpack-openocd-0.11.0-4-win-x64\bin\openocd.exe -c "set CPUTAPID 0" -f interface/stlink.cfg -f target/stm32f1x.cfg -c "transport select hla_swd" -c "program utils\generic_boot20_pc13.bin 0x08000000 verify reset exit"
+utils\openocd\xpack-openocd-0.11.0-4-win-x64\bin\openocd.exe -c "set CPUTAPID 0" -f interface/stlink.cfg -f target/stm32f1x.cfg -c "transport select hla_swd" -c "program utils\\generic_boot20_pc13.bin 0x08000000 verify reset exit"
 
 echo Check output if successful (Windows...)
 echo Move the BOOT1 Jumper back to position 0 and disconnect the BlueSCSI
