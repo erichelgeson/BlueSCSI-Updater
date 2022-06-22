13f8010395b14e6dca592de975baecba7e1a0120
Eric Helgeson <erichelgeson@gmail.com>
2022-06-22 07:52:42 -0500

    Update to v1.1-20220620

--- utils/flash-bluescsi-firmware-usb.bat
+++ utils/flash-bluescsi-firmware-usb.bat
@@ -1,4 +1,4 @@
-echo Flashing BlueSCSI-v1.1-20220404-USB.bin
+echo Flashing BlueSCSI-v1.1-20220620-STM32F1-USB.bin
 echo * Move BOOT1 to 1 (Jumper nearest the RESET button on the BluePill)
 echo * Connect via USB to the BluePill
 utils\dfu-util\dfu-util-win-x64\dfu-util-static.exe -d 0x1eaf:0x003 --download utils/BlueSCSI-v1.1-20220404-USB.bin --alt 2 -w
